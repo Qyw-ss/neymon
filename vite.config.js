24d4a2c44b063308288738231f8e3bd49578e3d1
iqbal muwafa <iqbalmuwafa@192.168.1.10>
2026-04-18 03:44:05 +0700

    fix: Add legacy plugin for Safari compatibility (es2015 transpilation)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    legacy({
+      targets: ['defaults', 'safari >= 14'],
+    })
   ],
+  build: {
+    target: 'es2015',
+  }
 })
